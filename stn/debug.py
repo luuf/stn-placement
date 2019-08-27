8c8dafefd5fb562c9e73b283f5a5b9e652a97306
luuf <finnveden.lukas@gmail.com>
2019-08-27 16:40:42 +0200

    some changes to the debugging file

--- stn/debug.py
+++ stn/debug.py
@@ -13,27 +13,30 @@ def test_transform(theta, length=17, im=None):
         im[0,0,length - 1,0] = 3
         im[0,0,length - 1,length - 1] = 4
         im[0,0,(length - 1)//2, (length - 1)//2] = 5
-    grid = F.affine_grid(t.tensor(theta).view(-1,2,3), (1,1,length,length))
+    grid = F.affine_grid(t.tensor(theta).view(-1,2,3), im.shape)
     transformed = F.grid_sample(im, grid)
     return transformed
 
 train,test = data.mnist(rotate=False,normalize=False,translate=False)
 test_im = train.dataset[0][0].view(1,1,28,28)
 plt.imshow(test_im[0,0])
+plt.show()
 
 identity = [1.,0,0,0,1,0]
-theta = t.tensor([1.,.5,-.2,0,1,0.5]).view(-1,2,3)
-distance = np.linalg.solve(theta[0,:,0:2], theta[0,:,2])
+theta = t.tensor([0.3,-1,-.5,1.3,-0.2,0.8]).view(-1,2,3)
+# distance = np.linalg.solve(theta[0,:,0:2], theta[0,:,2])
+distance = theta[0,:,2]
 print('distance', distance)
 
-plt.imshow(test_transform(theta,im=test_im)[0,0])
-plt.colorbar()
-plt.show()
 trans = test_transform([1., 0, distance[0], 0, 1, distance[1]],im=test_im)
 plt.imshow(trans[0,0])
 plt.colorbar()
 plt.figure()
 plt.imshow(test_transform([theta[0,0,0], theta[0,0,1], 0, theta[0,1,0], theta[0,1,1], 0], im=trans)[0,0])
+plt.colorbar()
+plt.figure()
+
+plt.imshow(test_transform(theta,im=test_im)[0,0])
 plt.colorbar()
 plt.show()
 #%%
