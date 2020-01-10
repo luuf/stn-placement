abe80c381503a4835be9c51cd5a225146adbe3c5
luuf <finnveden.lukas@gmail.com>
2020-01-10 23:21:41 +0100

    fixed bug, where the loss had to be summed across the batch

--- stn/learn.py
+++ stn/learn.py
@@ -202,7 +202,7 @@ def pretrain(epoch):
         #stn_angle = angles.angle_from_matrix(theta)
         #loss = t.mean(((stn_angle-moment_angle)%(2*np.pi))**2)
         det = torch.abs(torch.det(theta))
-        loss = torch.mean(torch.abs(theta-moment_theta)) + det + 1/det - 2
+        loss = torch.mean(torch.abs(theta-moment_theta)) + torch.mean(det + 1/det) - 2
         loss.backward()
         optimizer.step()
         history['train_loss'][epoch] += loss.item() * x.shape[0]
@@ -226,7 +226,7 @@ def pretest(epoch):
             theta = model(x)[:,0:2,0:2]
 
             det = torch.abs(torch.det(theta))
-            loss = torch.sum(torch.abs(theta-moment_theta)) + 4*(det+1/det-2)
+            loss = torch.sum(torch.abs(theta-moment_theta)) + 4*torch.sum(det + 1/det) - 8
             test_loss += loss.item()
     
     test_loss /= 4*len(test_loader.dataset)
